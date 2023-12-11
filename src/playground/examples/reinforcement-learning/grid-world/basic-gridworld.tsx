00d54965024a7f97d8303f17c04c78ac62c985ca
bifnudozhao <bifnudozhao@tencent.com>
2023-12-11 20:29:33 +0800

    add: save all and commit changes

--- src/playground/examples/reinforcement-learning/grid-world/basic-gridworld.tsx
+++ src/playground/examples/reinforcement-learning/grid-world/basic-gridworld.tsx
@@ -17,14 +17,10 @@ const BasicGridWorld: React.FC = () => {
   const gridLength = 100
   const margin = 0
   const size = 4
-  const board = [
-    [0, 1, 2, 3],
-    [4, 5, 6, 7],
-    [8, 9, 10, 11],
-    [12, 13, 14, 15],
-  ]
+  const board = _.times(size, row => _.times(size, col => row * size + col))
   const [currentState, setCurrentState] = React.useState(0)
   const [map, setMap] = React.useState({} as any)
+  const [policy, setPolicy] = React.useState({} as any)
   const [trainingProgress, setTrainingProgress] = React.useState(0)
 
   const renderBoard = () => (
@@ -43,7 +39,7 @@ const BasicGridWorld: React.FC = () => {
                 return (
                   <div
                     key={id}
-                    className="absolute border border-black flex justify-center items-center text-xm"
+                    className="absolute border border-black flex justify-center items-center text-lg text-blue-900"
                     style={{
                       top: margin,
                       left: margin,
@@ -56,8 +52,32 @@ const BasicGridWorld: React.FC = () => {
                     }}
                   >
                     {id === currentState && <div className="w-5 h-5 rounded-full bg-black"></div>}
-                    {map?.[id]?.type === 'goal' && <div className="text-lg">GOAL</div>}
-                    {map?.[id]?.type === 'hole' && <div className="text-lg">HOLE</div>}
+                    {map?.[id]?.type === 'goal' && <div className="text-lg text-black">GOAL</div>}
+                    {map?.[id]?.type === 'pit' && <div className="text-lg text-black">PIT</div>}
+                    <div
+                      className="absolute flex items-center top-2 left-1/2 -translate-x-1/2"
+                      style={{ opacity: policy?.[id]?.q_values[0] }}
+                    >
+                      <ArrowUpOutlined />
+                    </div>
+                    <div
+                      className="absolute flex items-center right-2 top-1/2 -translate-y-1/2"
+                      style={{ opacity: policy?.[id]?.q_values[1] }}
+                    >
+                      <ArrowRightOutlined />
+                    </div>
+                    <div
+                      className="absolute flex items-center bottom-2 left-1/2 -translate-x-1/2"
+                      style={{ opacity: policy?.[id]?.q_values[2] }}
+                    >
+                      <ArrowDownOutlined />
+                    </div>
+                    <div
+                      className="absolute flex items-center left-2 top-1/2 -translate-y-1/2"
+                      style={{ opacity: policy?.[id]?.q_values[3] }}
+                    >
+                      <ArrowLeftOutlined />
+                    </div>
                   </div>
                 )
               })}
@@ -147,10 +167,17 @@ const BasicGridWorld: React.FC = () => {
   }
 
   const train = async () => {
-    const res = await axios.get('http://127.0.0.1:5000/gridworld/train')
-    console.log(res)
+    const res = await axios.post('http://127.0.0.1:5000/gridworld/train', {
+      modelName: 'gridworld-q-learning-simple-network'
+    })
   }
 
+  const testModel = async () => {
+    const res = await axios.post('http://127.0.0.1:5000/gridworld/test_model', {
+      modelName: 'gridworld-q-learning-simple-network'
+    })
+    setPolicy(res.data.map)
+  }
 
   React.useEffect(() => {
     fetchMap()
@@ -173,6 +200,7 @@ const BasicGridWorld: React.FC = () => {
       <div onClick={(e) => step(2)}>down</div>
       <div onClick={(e) => step(3)}>left</div>
       <div onClick={(e) => train()}>train</div>
+      <div onClick={(e) => testModel()}>test model</div>
       <Progress percent={Math.ceil(trainingProgress * 100)} />
       <div className="mt-20" id="loss" />
     </div>
